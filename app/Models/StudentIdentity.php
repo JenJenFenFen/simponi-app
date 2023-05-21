a698df41e2275c41f98e3ad6b50b837e57942cba
Jennyver Seztiani Luxman <114470554+JenJenFenFen@users.noreply.github.com>
2023-05-22 00:46:31 +0700

    add fillable: ktp, school_last_education, id_classname, academic_year

--- app/Models/StudentIdentity.php
+++ app/Models/StudentIdentity.php
@@ -16,15 +16,19 @@ class StudentIdentity extends Model
         'country',
         'date_birth',
         'religion',
+        'ktp',
         'address',
         'status',
         'number_phone',
         'email',
         'last_education',
         'major_last_education',
+        'school_last_education',
         'major',
         'study_program',
+        'id_classname',
         'semester',
+        'academic_year',
         'photo'
     ];
 }
