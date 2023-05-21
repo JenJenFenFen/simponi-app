60b7fee5913c981f3f292dbaff8e741e2133c1f6
Jennyver Seztiani Luxman <114470554+JenJenFenFen@users.noreply.github.com>
2023-05-21 20:42:28 +0700

    create new collector (fetchMahasiswa and sendMahasiswa)

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Symfony\Component\HttpFoundation\Request as HttpFoundationRequest;
 
 class AdminController extends Controller
@@ -15,6 +18,17 @@ class AdminController extends Controller
         return view('admin.daftar_mahasiswa', [
             "title" => "mahasiswa"
         ]);
+    }
+
+    public function fetchMahasiswa(Request $request) {
+        $jurusan = $request->input('jurusan');
+        $students = DB::table('student_identities')->where('major', $jurusan)->count();
+
+        return $students;
+    }
+
+    public function sendMahasiswa(Request $request) {
+        dd($request);
     }
 
     public function daftarDosen() {
