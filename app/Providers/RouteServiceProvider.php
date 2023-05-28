f5c6b63aabf7b5b038a2093caec57a243956d717
Jennyver Seztiani Luxman <114470554+JenJenFenFen@users.noreply.github.com>
2023-05-29 01:24:42 +0700

    split route file

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -32,7 +32,12 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
-                ->group(base_path('routes/web.php'));
+                ->prefix('admin')
+                ->group(base_path('routes/admin.php'));
+            
+            Route::middleware('web')
+                ->prefix('student')
+                ->group(base_path('routes/student.php'));
         });
     }
 
