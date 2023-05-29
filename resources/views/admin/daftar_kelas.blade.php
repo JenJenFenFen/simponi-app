a8a95769153a5628829e4090718bd59680e0cb9c
Jennyver Seztiani Luxman <114470554+JenJenFenFen@users.noreply.github.com>
2023-05-29 23:13:50 +0700

    add new error message to nama kelas's input and add disabled prop at checklist

--- resources/views/admin/daftar_kelas.blade.php
+++ resources/views/admin/daftar_kelas.blade.php
@@ -21,7 +21,9 @@
             <div class="grid grid-cols-12 gap-4 mb-6">
                 <div class="col-start-1 col-end-4">
                     <label for="nama_kelas" class="block mb-2 text-sm font-medium text-gray-900">Nama Kelas</label>
-                    <input type="text" id="nama_kelas" class="bg-gray-50 border text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 border-gray-300">
+                    <input type="text" id="nama_kelas" class="bg-gray-50 border text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 uppercase border-gray-300" maxlength="4">
+                    <span class="font-medium mt-2 text-sm text-red-600" hidden>Masih kosong! Silakan diisi!</span>
+                    <span id="confirmLengthKelas" class="font-medium mt-2 text-sm text-red-600" hidden>Format kelas salah! Silakan diperbaiki!</span>
                 </div>
                 <div class="mt-7">
                     <button type="button" id="btnAdd" class="text-white bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Tambah</button>
@@ -59,9 +61,9 @@
                 {{-- checklist --}}
                 <div class="flex items-start mt-6">
                     <div class="flex items-center h-5">
-                    <input id="remember" type="checkbox" class="w-4 h-4 border rounded bg-gray-50 focus:ring-3 focus:ring-blue-300" required>
+                    <input id="remember" type="checkbox" class="w-4 h-4 border rounded bg-gray-50 focus:ring-3 focus:ring-blue-300" required disabled>
                     </div>
-                    <label for="remember" class="ml-2 text-sm font-medium text-gray-900">Semua data kelas yang diisi sudah benar.</label>
+                    <label for="remember" class="ml-2 text-sm font-medium text-gray-200">Semua data kelas yang diisi sudah benar.</label>
                 </div>
                 <button type="button" id="btnProcess" class="mt-8 text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center bg-gray-200 cursor-not-allowed" disabled>Proses</button>
             </form>
