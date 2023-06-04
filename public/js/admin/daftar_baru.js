8a9ce8f45257cad127d71383c5731331c1805fd6
Jennyver Seztiani Luxman <114470554+JenJenFenFen@users.noreply.github.com>
2023-06-05 00:02:18 +0700

    fix code (prop to attr)

--- public/js/admin/daftar_baru.js
+++ public/js/admin/daftar_baru.js
@@ -231,13 +231,13 @@ $("#remember").on("change", function () {
         $("#btnProcess").removeClass("cursor-not-allowed")
         $("#btnProcess").addClass("bg-blue-700")
         $("#btnProcess").addClass("hover:bg-blue-800")
-        $("#btnProcess").prop("disabled", false)
+        $("#btnProcess").removeAttr("disabled")
     }
     else {
         $("#btnProcess").removeClass("bg-blue-700")
         $("#btnProcess").removeClass("hover:bg-blue-800")
         $("#btnProcess").addClass("bg-gray-200")
         $("#btnProcess").addClass("cursor-not-allowed")
-        $("#btnProcess").prop("disabled", true)
+        $("#btnProcess").attr("disabled", true)
     }
 })
