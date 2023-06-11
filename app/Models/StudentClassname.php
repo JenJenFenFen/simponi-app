2f32b533dfbb20e4fb24bab26d3c39eacd60d355
Jennyver Seztiani Luxman <114470554+JenJenFenFen@users.noreply.github.com>
2023-06-11 23:32:11 +0700

    add student identity and classname relationship

--- app/Models/StudentClassname.php
+++ app/Models/StudentClassname.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\StudentIdentity;
+use App\Models\Classname;
 
 class StudentClassname extends Model
 {
@@ -13,4 +16,12 @@ class StudentClassname extends Model
         'id_classname',
         'id_student_identity'
     ];
+
+    public function student_identities (): BelongsTo {
+        return $this->belongsTo(StudentIdentity::class, 'id_student_identity');
+    }
+
+    public function classnames (): BelongsTo {
+        return $this->belongsTo(Classname::class, 'id_classname');
+    }
 }
