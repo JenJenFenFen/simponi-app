014815e800fc8ee8573bdf90fac17f13afea77df
Jennyver Seztiani Luxman <114470554+JenJenFenFen@users.noreply.github.com>
2023-06-11 21:05:42 +0700

    Create UserLoginSeeder.php

---
+++ database/seeders/UserLoginSeeder.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\UserLogin;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Crypt;
+
+class UserLoginSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        UserLogin::create([
+            'email' => 'fenjenjen@gmail.com',
+            'password' => Crypt::encryptString('selatpanjang'),
+            'id_rule' => 2
+        ]);
+
+        UserLogin::create([
+            'email' => 'jefryofficial@gmail.com',
+            'password' => Crypt::encryptString('selatpanjang'),
+            'id_rule' => 2
+        ]);
+
+        UserLogin::create([
+            'email' => 'ezrarexy@gmail.com',
+            'password' => Crypt::encryptString('selatpanjang'),
+            'id_rule' => 2
+        ]);
+
+        UserLogin::create([
+            'email' => 'derryalam@gmail.com',
+            'password' => Crypt::encryptString('selatpanjang'),
+            'id_rule' => 3
+        ]);
+
+        UserLogin::create([
+            'email' => 'tinalia@gmail.com',
+            'password' => Crypt::encryptString('selatpanjang'),
+            'id_rule' => 3
+        ]);
+
+        UserLogin::create([
+            'email' => 'timotiusa@gmail.com',
+            'password' => Crypt::encryptString('selatpanjang'),
+            'id_rule' => 3
+        ]);
+        
+    }
+}
