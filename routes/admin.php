760a5412be5a4d600fd085269620f623da1984da
Jennyver Seztiani Luxman <114470554+JenJenFenFen@users.noreply.github.com>
2023-06-11 21:11:08 +0700

    add material and jadwal route

--- routes/admin.php
+++ routes/admin.php
@@ -8,6 +8,10 @@ Route::get('/daftar-mahasiswa', [AdminController::class, 'daftarMahasiswa'])->na
 Route::post('/send-mahasiswa', [AdminController::class, 'sendMahasiswa'])->name('sendMahasiswa');
 Route::get('/daftar-dosen', [AdminController::class, 'daftarDosen'])->name('daftarDosen');
 Route::post('/send-dosen', [AdminController::class, 'sendDosen'])->name('sendDosen');
+Route::get('/daftar-material', [AdminController::class, 'daftarMaterial'])->name('daftarMaterial');
+Route::post('/send-material', [AdminController::class, 'sendMaterial'])->name('sendMaterial');
+Route::get('/daftar-jadwal', [AdminController::class, 'daftarJadwal'])->name('daftarJadwal');
+Route::post('/send-jadwal', [AdminController::class, 'sendJadwal'])->name('sendJadwal');
 Route::get('/daftar-kelas', [AdminController::class, 'daftarKelas'])->name('daftarKelas');
 Route::post('/send-kelas', [AdminController::class, 'sendKelas'])->name('sendKelas');
 Route::get('/input-mahasiswa-kelas', [AdminController::class, 'inputMahasiswaKelas'])->name('inputMahasiswaKelas');
